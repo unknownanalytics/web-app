c052eae4916ca7a2274b0235aa1b976471d0bb4c
fathallah.houssem <houssem.fathallah@devoteam.com>
2019-10-25 15:02:14 +0200

    add secrect key base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  puts "ccccccccccccccccccccc"
-
-  config.secret_key_base =  ENV["UNK_ANA_SECRET_KEY_BASE"]
+  config.secret_key_base =  ENV['UNK_ANA_SECRET_KEY_BASE']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
